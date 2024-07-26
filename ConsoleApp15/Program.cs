79307f7a4157ce1a6a00fff6e5c79ae5baa69aff
Rohat Güneş <rohatgns44@gmail.com>
2024-07-27 01:56:35 +1000

    Add project files.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Lütfen bir limit değeri giriniz: ");
+int limit = Convert.ToInt32(Console.ReadLine());
+int sayac = 0;
+
+do
+{
+    Console.WriteLine("ben bir patikalıyım");
+    sayac++;
+
+}while (sayac <= limit);
+{
+    // do-while döngüsü ile:
+
+}
